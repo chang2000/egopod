6a765ce08ed78d26014c432decda682767541be1
1155124470 <925955711@qq.com>
2021-04-17 11:22:02 +0800

    noteString

--- backend/routes/api/note.js
+++ backend/routes/api/note.js
@@ -6,8 +6,8 @@ const sqlQuery = require('../../db')
 
 router.get('/addts', async (req, res) => {
     //console.log(req.query)
-    let sqlStr = "insert ignore into timestamp (useremail,podcastID,episodeID,timestamp) values (?,?,?,?);"
-    let arr = [req.query.userEmail, req.query.podcastID, req.query.episodeID, req.query.timeStamp]
+    let sqlStr = "insert into note (useremail,podcastID,episodeID,timestamp,noteString) values (?,?,?,?,?);"
+    let arr = [req.query.userEmail, req.query.podcastID, req.query.episodeID, req.query.timeStamp,req.query.noteString]
     let result = await sqlQuery(sqlStr, arr)
     console.log(result)
     res.status(200).send()
@@ -15,9 +15,20 @@ router.get('/addts', async (req, res) => {
 
 router.get('/delts', async (req, res) => {
     console.log(req.query)
-    let sqlStr = "delete from timestamp where useremail=? and podcastID=? and episodeID=? and timestamp = ?;"
-    let arr = [req.query.userEmail, req.query.podcastID, req.query.episodeID, req.query.timeStamp]
-    let result = await sqlQuery(sqlStr, arr)
+    let sqlStr = "delete from note where useremail= '"+ req.query.userEmail +"'"
+    if (req.query.podcastID != undefined) {
+        sqlStr = sqlStr + "AND podcastID = '" + req.query.podcastID + "'"
+    }
+    if (req.query.episodeID != undefined) {
+        sqlStr = sqlStr + "AND podcastID = '" + req.query.episodeID + "'"
+    }
+    if (req.query.timestamp != undefined) {
+        sqlStr = sqlStr + "AND podcastID = '" + req.query.timeStamp + "'"
+    }
+    sqlStr = sqlStr + ';'
+    console.log(sqlStr)
+    let result = await sqlQuery(sqlStr)
+    console.log(result)
     res.status(200).send()
 })
 
@@ -25,7 +36,7 @@ router.get('/queryAll', async (req, res) => {
 
     //console.log(req.query)
     //let sqlStr = "select * from timestamp where useremail=? and podcastID=? and episodeID=?;"
-    let sqlStr = "select podcastID,episodeID,timestamp from timestamp where userEmail = '" + req.query.userEmail + "';"
+    let sqlStr = "select podcastID,episodeID,timestamp,noteString from note where userEmail = '" + req.query.userEmail + "'"
     //console.log(sqlStr)
     if (req.query.podcastID != undefined) {
         sqlStr = sqlStr + "AND podcastID = '" + req.query.podcastID + "'"
@@ -38,12 +49,9 @@ router.get('/queryAll', async (req, res) => {
     }
     //let arr = [req.query.useremail,req.query.podcastID,req.query.episodeID,req.query.timestamp]
     //console.log(arr)
+    sqlStr = sqlStr + ';'
     let result = await sqlQuery(sqlStr)
-    //console.log(result)
-    // let result_new = result.map((items)=>{
-    //     return items[]
-    // })
-    let reply = { userEmail: req.query.userEmail, timeStamps: result }
+    let reply = { userEmail: req.query.userEmail, Note: result }
     res.status(200).json(reply)
 })
 
